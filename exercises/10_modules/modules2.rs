370bf5174ec327b64ecd2e1129785f1212ac7e36
GuCMartins <102696637+GuCMartins@users.noreply.github.com>
2024-01-07 17:02:23 -0300

    Estudar com mais calma o uso das Options

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -7,28 +7,31 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
-
+    
     mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
-
+    
     mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
+    pub use self::fruits::PEAR as fruit1;       //aplica outro nome para o item ja dentro do modulo fruits
+    pub use self::veggies::CUCUMBER as veggie1; //aplica outro nome para o item ja dentro do modulo veggies
+
+    pub use self::fruits::APPLE as fruit2;      //aplica outro nome para o item ja dentro do modulo fruits
+    pub use self::veggies::CARROT as veggie2;   //aplica outro nome para o item ja dentro do modulo veggies
 }
 
 fn main() {
     println!(
-        "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        "favorite snacks: {} and {}, but I also like {} and {}",
+        delicious_snacks::fruit1,//precisa ter o nome do modulo antes do item pois
+        delicious_snacks::veggie1,//ele esta sendo especificado dentro do modulo
+        delicious_snacks::fruit2,//e nao no escopo global
+        delicious_snacks::veggie2,
     );
 }
